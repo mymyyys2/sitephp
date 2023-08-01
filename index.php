d449d5dd05228c1445d15cc0f20dcd4817c7436e
Nuemo <116648601+mymyyys2@users.noreply.github.com>
2023-08-01 14:40:11 -0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<?php
+echo "ja' podeh Al Mossahr?";
+$turma2 = "vamos desenvolver website em php";
+//o comando echo imprime a  mensagem na tela :D
+echo "<br>".$turma2;
+$x = 34 /* + 15 */ + 35;
+echo "<br>".$x;
+$txt = "ola mundo";
+$x = 5;
+$y = 10.5;
+echo "<br>".$txt." x = ".$x." y = ".$y;
+?>
+
+</body>
+</html>
